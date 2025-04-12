2b13f7023ea7417904729bdf6aa913d5b1682fd6
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-04-12 16:02:25 +0530

    backend and fetch function

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -3,12 +3,19 @@ import { useSelector, useDispatch } from "react-redux";
 
 import "./ToDoList.css";
 import { actions, todoSelector } from "../../redux/reducers/todoReducer";
+import { useEffect } from "react";
 
 function ToDoList() {
 
   const todos = useSelector(todoSelector);
 
   const disptach = useDispatch();
+
+  useEffect(() => {
+    fetch("http://localhost:4100/api/todos").then(res => res.json()).then(parsedJson => console.log(parsedJson)
+    ).catch(err => console.log(err)
+    )
+  }, [])
   // const todos= store.getState().todos;
 
   return (
